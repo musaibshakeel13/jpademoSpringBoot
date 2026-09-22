3766845bd5248b40e4d698cea4c36be4ab753e7f
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-22 20:06:59 +0400

    Update UserClassSpringBoot.java

--- src/main/java/com/example/jpademo/UserClassSpringBoot.java
+++ src/main/java/com/example/jpademo/UserClassSpringBoot.java
@@ -1,1 +1,41 @@
+package com.example.jpademo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class UserClassSpringBoot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+    private  String name;
+//    private  String email;
+
+
+    public UserClassSpringBoot() {
+    }
+
+    public UserClassSpringBoot(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
 
